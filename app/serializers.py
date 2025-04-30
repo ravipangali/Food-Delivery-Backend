b1736746de17aaf1026ef0f1c5363fd38b882aef
Infelo <infelo453@gmail.com>
2025-04-30 22:07:20 +0545

    Serializer and Api

--- app/serializers.py
+++ app/serializers.py
@@ -6,49 +6,58 @@ from rest_framework import serializers
 class OrganizationSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrganizationSetting
+        depth = 10
         fields = '__all__'
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
+        depth = 10
         fields = '__all__'
 
 class FoodSerializer(serializers.ModelSerializer):
     ratings = RatingSerializer(many=True, read_only=True).data
     class Meta:
         model = Food
+        depth = 10
         fields = ('id', 'name', 'image', 'price', 'discount', 'food_type', 'category', 'restaurant', 'ratings')
 
 class FoodCategorySerializer(serializers.ModelSerializer):
     foods = FoodSerializer(many=True, read_only=True).data
     class Meta:
         model = FoodCategory
+        depth = 10
         fields = ('id', 'name', 'image', 'foods')
 
 class RestaurantSerializer(serializers.ModelSerializer):
     foods = FoodSerializer(many=True, read_only=True).data
     class Meta:
         model = Restaurant
+        depth = 10
         fields = ('id', 'name', 'logo', 'address', 'phone', 'foods')
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
+        depth = 10
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
     order_items = OrderItemSerializer(many=True, read_only=True).data
     class Meta:
         model = Order
+        depth = 10
         fields = ('id', 'status', 'customer', 'address', 'latitude', 'longitude', 'phone', 'sub_total', 'delivery_charge', 'total', 'note', 'created_at', 'order_items')
 
 class CustomerSerializer(serializers.ModelSerializer):
     orders = OrderSerializer(many=True, read_only=True).data
     class Meta:
         model = Customer
+        depth = 10
         fields = ('id', 'name', 'phone', 'address', 'image', 'orders')
 
 class BannerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
+        depth = 10
         fields = '__all__'
--- food_delivery_backend/settings.py
+++ food_delivery_backend/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +60,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'food_delivery_backend.urls'
 
